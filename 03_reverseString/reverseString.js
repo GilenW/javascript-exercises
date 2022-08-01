ccc811e3bc809229157c6f5c8971f9073b890870
GilenFC <gwuhou@ucsd.edu>
2022-07-31 21:39:51 -0700

    practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = '';
+    const result = [];
 
+    let arrayOfString = string.split(" ");
+    let index = arrayOfString.length - 1;
+    for(let i = 0; i < arrayOfString.length; i++){
+        for(let j = arrayOfString[i].length - 1; j >= 0 ; j--){
+            reverseString += arrayOfString[i][j];
+        }
+        result[index] = reverseString;
+        index--;
+        reverseString = '';
+    }
+    return result.join(" ");
 };
 
 // Do not edit below this line
